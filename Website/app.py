433bd5c0e4d444e0e631c8f9a3be9defe18ad9cd
singhm50 <singhm50@coventry.ac.uk>
2023-03-17 21:33:13 +0000

    added base.html file

--- Website/app.py
+++ Website/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask
+from markupsafe import escape
+from flask import url_for
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import abort
+from flask import make_response
+from flask import session
+from flask import flash
+import sqlite3
+from flask import g
+import functools
+import os
 
 app = Flask(__name__)
+app.secret_key = 'book_shop'
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
