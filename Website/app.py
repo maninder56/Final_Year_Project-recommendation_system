8eec6a7b5b1bff53f7bc9cdf0c3012d801e58e45
singhm50 <singhm50@coventry.ac.uk>
2023-03-24 22:41:21 +0000

    login page added

--- Website/app.py
+++ Website/app.py
@@ -18,9 +18,42 @@ app.secret_key = 'book_shop'
 
 @app.route("/")
 def home():
+    # user already logged in 
+    if "user" in session:
+       flash(f"Already logged: {session['user']}", "info")
     return render_template("index.html")
 
 
+
+# Login system 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   if request.method == 'POST':
+     return do_the_login(request.form['uname'], request.form['pwd'])
+   else:
+      if "user" in session:
+         return redirect(url_for('home'))
+      return show_the_login_form()
+
+def show_the_login_form():
+   return render_template('login.html',login_form=url_for('login'))
+
+
+
+def do_the_login(u,p):
+   if (p == 'password'):
+      session['user'] = u
+      return redirect(url_for('home'))
+   else:
+      abort(403)
+
+# logout 
+@app.route('/logout')
+def logout():
+    session.pop('user', None) 
+    flash("You have been logged out","info") 
+    return redirect(url_for('home')) 
 
 
 
