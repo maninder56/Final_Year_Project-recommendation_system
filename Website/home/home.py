08e0ca686a1dc11dd336227c43f6f9a1cd0e4dae
singhm50 <singhm50@coventry.ac.uk>
2023-03-25 19:32:37 +0000

    home page seperated from main app

---
+++ Website/home/home.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from markupsafe import escape
+from flask import url_for
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import abort
+from flask import make_response
+from flask import session
+from flask import flash
+import sqlite3
+from flask import g
+import functools
+import os
+
+from flask import Blueprint
+
+home_bp = Blueprint("home", __name__,
+                  template_folder="templates")
+
+@home_bp.route("/")
+def home():
+    # user already logged in 
+    if "user" in session:
+       #send this info to home page so login can be changed dynamically
+       flash(f"logged in as: {session['user']}", "info")
+
+    # load books 
+
+    # button to add books to cart 
+
+
+
+
+    return render_template("home.html")
